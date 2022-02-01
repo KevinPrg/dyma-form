6a2714c0d6cc2f05ecb13e328a2047a97b25c365
KevinPrg <pininfarina47@gmail.com>
2022-02-01 09:05:24 +0100

    final first

--- dyma-form/src/App.js
+++ dyma-form/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Composant from './composant';
 
 function App() {
   return (
     <div className="App">
-      <Composant name = "coucou"/>
+
     </div>
   );
 }
--- dyma-form/src/composant.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Composant1 = (props) => {
-    return <h1>Hello World {props.name}</h1>
-}
-
-export default Composant1;
